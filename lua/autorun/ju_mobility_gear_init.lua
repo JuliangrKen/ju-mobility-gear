6ae1fd9d5335b609e950052aa7a7aeb630e8dba3
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-04-25 15:01:53 +0300

    Настроил инит файл

---
+++ lua/autorun/ju_mobility_gear_init.lua
@@ -0,0 +1,48 @@
+-- by Julik#8946
+
+-- addon global tables
+
+ju = ju or {}
+ju.mobility_gear = ju.mobility_gear or {}
+
+
+-- include functions:
+
+local rDir = 'ju_mobility_gear/'
+
+local addSv = SERVER and include or function() end
+local addCl = SERVER and AddCSLuaFile or include
+local addSh = function(...)
+    addSv(...)
+    addCl(...)
+end
+
+local addFile = function(name, dir)
+    local prefix = string.Left(name, 3)
+    local path = dir..name
+
+    if prefix == 'sv_' then
+        addSv(path)
+        return
+    end
+
+    if prefix == 'cl_' then
+        addCl(path)
+        return
+    end
+
+    addSh(path)
+end
+
+local addDir = function(name)
+    local dir = rDir..name..'/'
+    local files, _ = file.Find(dir..'*', 'LUA')
+    
+    for _, v in ipairs(files) do
+        if !string.EndsWith(v, '.lua') then return end
+        addFile(v, dir)
+    end
+end
+
+-- includes functions:
+
