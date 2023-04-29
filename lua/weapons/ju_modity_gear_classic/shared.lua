76eda15e0d415273462f41f05854182780b7d673
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-04-29 18:52:18 +0300

    Добавил ресурсы и авторство

--- lua/weapons/ju_modity_gear_classic/shared.lua
+++ lua/weapons/ju_modity_gear_classic/shared.lua
@@ -1,12 +1,15 @@
 
 local mg = ju.mobility_gear
 local cfg = mg.cfg
+local resources = mg.resources
 
 
 SWEP.Base = 'ju_modity_gear_base'
 
 -- Q-menu
 SWEP.PrintName = 'Классический УМП'
+SWEP.Author = 'Julik#8946'
 SWEP.Category = cfg.q_menu_category
 SWEP.Spawnable = true
 
+SWEP.IconOverride = resources.weaponIcons.classic
