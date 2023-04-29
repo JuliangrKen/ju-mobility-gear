b1b0e9db6edb3a2e40ffa718ff2917963f4bd9fd
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-04-29 20:06:42 +0300

    Сформулировал код для полёта

--- lua/ju_mobility_gear/core/sv_mg_flying.lua
+++ lua/ju_mobility_gear/core/sv_mg_flying.lua
@@ -1,1 +1,27 @@
-print 'was inited!'
+
+local mg = ju.mobility_gear
+
+local function fly(ply, mv)
+    
+end
+
+local function flyWithHooks(ply, mv)
+    
+end
+
+local switchTable = {
+    [4] = fly,
+    [5] = flyWithHooks,
+}
+
+hook.Add('Move', 'ju_mobility_gear_move', function(ply, mv)
+
+    local state = ply:GetMGState() or 0
+
+    if state = 0 then
+        return
+    end
+
+    switchTable[state](ply, mv)
+
+end)
