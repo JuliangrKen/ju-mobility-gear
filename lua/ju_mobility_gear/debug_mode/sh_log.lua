9be0efb5b1c377bc8b67eaa1ac45c89827ec89f4
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-04-29 21:41:16 +0300

    Добавил логгер

---
+++ lua/ju_mobility_gear/debug_mode/sh_log.lua
@@ -0,0 +1,19 @@
+
+local mg = ju.mobility_gear
+local _os = os
+local time = _os.date
+
+function mg.log(message)
+    
+    local tag = '[ JuMG - ' .. time('%H:%M:%S') .. ' ] >>   '
+
+    if istable(message) then
+        print(tag)
+        print '\n\n\n<< START >>\n\n\n'
+        PrintTable(message)
+        print '\n\n\n<< END >>\n\n\n'
+    end
+
+    print(tag .. message)
+
+end
