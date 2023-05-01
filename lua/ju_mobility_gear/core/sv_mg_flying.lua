92b8120f9b5193a57424abb284293f5b5101d7b9
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-05-01 16:13:47 +0300

    Первые шаги в реализации полёта

--- lua/ju_mobility_gear/core/sv_mg_flying.lua
+++ lua/ju_mobility_gear/core/sv_mg_flying.lua
@@ -1,15 +1,54 @@
 
+local CurTime = CurTime
+local math = math
+local random = math.random
+local Vector = Vector
+local Angle = Angle
+
 local mg = ju.mobility_gear
+
+local function getDataFromMV(mv)
+
+    local angle = mv:GetMoveAngles()
+    local position = mv:GetOrigin()
+    local velocity = mv:GetVelocity()
+
+    return angle, position, velocity
+
+end
+
+local function updateMVData(mv, angle, position, velocity)
+
+    mv:SetMoveAngles(angle)
+	mv:SetOrigin(position)
+    mv:SetVelocity(velocity)
+
+end
 
 local function moveWithMG(ply, mv)
 
     print 'moveWithMG'
-    
+
 end
+
+local jumpCooldown = 3
+local minJumpBoost = 128
+local maxJumpBoost = 256
 
 local function moveWithMGMode(ply, mv)
     
-    print 'moveWithMGMode'
+    local angle, position, velocity = getDataFromMV(mv)
+
+    if !(mv:KeyDown(IN_JUMP) and mv:KeyDown(IN_SPEED)) then return end
+
+    if CurTime() - (ply.lastMGJump or 0) < jumpCooldown then return end
+    ply.lastMGJump = CurTime()
+
+    velocity = Vector(0, 0, 1) * random(minJumpBoost, maxJumpBoost)
+
+    updateMVData(mv, angle, position, velocity)
+
+    ply:SetMGState(4)
 
 end
 
@@ -21,7 +60,13 @@ end
 
 local function fly(ply, mv)
     
-    print 'fly'
+    local angle, position, velocity = getDataFromMV(mv)
+
+    mv:SetMoveAngles(angle)
+	mv:SetOrigin(position)
+    mv:SetVelocity(velocity)
+
+
 
 end
 
@@ -50,7 +95,7 @@ local switchTable = {
 
 hook.Add('Move', 'ju_mobility_gear_move', function(ply, mv)
 
-    local state = ply:GetMGState() or 0
+    local state = ply:GetMGState()
 
     if state == 0 then
         return
@@ -63,5 +108,14 @@ hook.Add('Move', 'ju_mobility_gear_move', function(ply, mv)
     end
 
     switchFunc(ply, mv)
+
+
+end)
+
+hook.Add('GetFallDamage', 'ju_mobility_gear_removeFallDamage', function(ply, speed)
+    
+    if ply:GetMGState() != 0 then
+        return 0
+    end
 
 end)
