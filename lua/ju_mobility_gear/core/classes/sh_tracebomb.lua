7c85eef46389c9dbf4d6ecab5f7e549307000b27
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-05-08 23:21:54 +0300

    Обновил многое, добавил TODO и комменты

--- lua/ju_mobility_gear/core/classes/sh_tracebomb.lua
+++ lua/ju_mobility_gear/core/classes/sh_tracebomb.lua
@@ -10,39 +10,102 @@ local defaultNumVertexesCircle = 10
 
 local TRACE_BOMB = {}
 
---[[
-    
-    public:
-
-    Radius
-    NumVertexesCircle
-
-]]
-
 TRACE_BOMB.__index = TRACE_BOMB
 
-function TRACE_BOMB:new(radius, numVertexesCircle)
+--[[
+    TODO:
+    1. фикс - увеличение/уменьшение сферы.
+    2. добавить - полусфера и прочие об плоскость.
+]]
+
+--[[
+    radius : Num
+    pos : Vector
+    numVertexesCircle : Num
+
+    return new TRACE_BOMB table
+]]
+function TRACE_BOMB:new(radius, pos, numVertexesCircle)
     
     local heir = setmetatable({}, self)
 
     heir.__index = self
 
-    heir.Radius = radius
-    heir.NumVertexesCircle = numVertexesCircle or defaultNumVertexesCircle
+    heir.radius = radius
+    heir.pos = pos
+    heir.numVertexesCircle = numVertexesCircle or defaultNumVertexesCircle
 
     return heir
 
 end
 
 
-function TRACE_BOMB:SetPos(pos)
+-- Position methods
 
+--[[
+    pos : Vector
+
+    return this table
+]]
+function TRACE_BOMB:SetPos(pos)
+    
     if self.pos != pos then
+
+        local lastPos = self:GetPos()
+        
+        self.pos = pos
+
+        if self.vertexes then
+        
+            self:updatePos(lastPos)
+
+        else
+    
+            self:updateVertexes()
+
+        end
+
+    end
+
+    return self
+
+end
+
+--[[
+    return pos : Vector
+]]
+function TRACE_BOMB:GetPos()
+
+    return self.pos or Vector(0, 0, 0)
+    
+end
+
+
+-- Scale methods
+
+--[[
+    radius : Num
+
+    return this table
+]]
+function TRACE_BOMB:SetRadius(radius)
+
+    print(radius)
+
+    if self.radius != radius then
+
+        local lastRadius = self:GetRadius()
+
+        self.radius = radius
         
         if self.vertexes then
         
-            self:UpdateScale(pos / self.pos)
+            self:updateScale(lastRadius)
+
+        else
     
+            self:updateVertexes()
+
         end
     
         self.pos = pos
@@ -53,13 +116,24 @@ function TRACE_BOMB:SetPos(pos)
 
 end
 
-function TRACE_BOMB:GetPos()
+--[[
+    return radius : Num
+]]
+function TRACE_BOMB:GetRadius()
 
-    return self
-    
+    return self.radius or 1
+
 end
 
+-- TODO: update Rotation methods
 
+-- Rotation methods
+
+--[[
+    sphereAxis : Vector
+
+    return this table
+]]
 function TRACE_BOMB:SetSphereAxis(sphereAxis)
     
     self.sphereAxis = sphereAxis
@@ -67,12 +141,20 @@ function TRACE_BOMB:SetSphereAxis(sphereAxis)
 
 end
 
+--[[
+    return sphereAxis : Vector
+]]
 function TRACE_BOMB:GetSphereAxis()
     
     return self.sphereAxis or Vector(0, 0, 0)
 
 end
 
+--[[
+    isX : bool
+
+    return this table
+]]
 function TRACE_BOMB:SetHorizontal(isX)
     
     self:SetSphereAxis(isX and Vector(1, 0, 0) or Vector(0, 1, 0))
@@ -80,6 +162,9 @@ function TRACE_BOMB:SetHorizontal(isX)
 
 end
 
+--[[
+    return this table
+]]
 function TRACE_BOMB:SetVertical()
     
     self:SetSphereAxis(Vector(0, 0, 1))
@@ -87,28 +172,43 @@ function TRACE_BOMB:SetVertical()
 
 end
 
+--[[
+    Trace methods
 
+    Wiki Page:
+    https://wiki.facepunch.com/gmod/Structures/Trace
+]]
+
+--[[
+    filter : Entity or table {Entity} or function(Entity)
+    
+    return this table
+]]
 function TRACE_BOMB:SetTraceFilter(filter)
 
     self.filter = filter
-    return filter
+    return self
     
 end
 
+--[[
+    return filter : Entity or table {Entity} or function(Entity)
+]]
 function TRACE_BOMB:GetTraceFilter()
     
-    return filter
+    return self.filter
 
 end
 
 
 --[[
-    function samplingConditionsFunc(trace, pos)
+    samplingConditionsFunc : function(trace : Table, pos : Vector)
 
-    trace - table (Structures/TraceResult)
-    pos - TRACE_BOMB.Pos
+    Table:
+        https://wiki.facepunch.com/gmod/Structures/TraceResult
+
+    return this table
 ]]
-
 function TRACE_BOMB:SetSamplingConditionsFunc(samplingConditionsFunc)
     
     self.samplingConditionsFunc = samplingConditionsFunc
@@ -116,13 +216,18 @@ function TRACE_BOMB:SetSamplingConditionsFunc(samplingConditionsFunc)
 
 end
 
+--[[
+    return samplingConditionsFunc : function(trace : Table, pos : Vector)
+]]
 function TRACE_BOMB:GetSamplingConditionsFunc()
     
     return self.samplingConditionsFunc
 
 end
 
-
+--[[
+    return this table
+]]
 function TRACE_BOMB:SetTakeEverything()
     
     self.takeEverything = true
@@ -130,6 +235,9 @@ function TRACE_BOMB:SetTakeEverything()
 
 end
 
+--[[
+    return this table
+]]
 function TRACE_BOMB:SetTakeFirstOne()
 
     self.takeEverything = false
@@ -137,7 +245,101 @@ function TRACE_BOMB:SetTakeFirstOne()
 
 end
 
+--[[
+    return Trace or Table { Trace }
+]]
+function TRACE_BOMB:Boom()
+    
+    if !self.vertexes then
+        self:updateVertexes()
+    end
 
+    local resultTable = self.takeEverything and {}
+
+    for _, v in ipairs(self.vertexes) do
+        
+        local trace = util.TraceLine({
+            
+            start = self:GetPos(),
+            endpos = v,
+
+            filter = self.filter,
+        
+        })
+
+        if !(trace and trace.Hit) then continue end
+
+        if self.samplingConditionsFunc and !self.samplingConditionsFunc(trace, self:GetPos()) then continue end
+
+        if !resultTable then return trace end
+
+        resultTable[#resultTable + 1] = trace 
+
+    end
+
+    return resultTable
+
+end
+
+
+if CLIENT then
+
+    --[[
+        color : Color
+    ]]
+    function TRACE_BOMB:DrawShere(color)
+        
+        if !self.vertexes then return end
+
+        local last
+        for k, v in ipairs(self.vertexes) do
+            
+            if last then
+                render.DrawLine(last, v, color or defaultColor, true)
+            end
+
+            last = v
+
+        end
+
+    end
+
+    --[[
+        color : Color
+    ]]
+    function TRACE_BOMB:DrawPaintedShere(color)
+        
+        render.SetColorMaterial()
+        render.DrawSphere(self:GetPos(), self.radius, self.numVertexesCircle, self.numVertexesCircle, color or defaultColor)
+
+    end
+
+    --[[
+        color : Color
+    ]]
+    function TRACE_BOMB:DrawTraces(color)
+    
+        if !self.vertexes then return end
+
+        for _, v in ipairs(self.vertexes) do
+            
+            render.SetColorMaterial()
+            render.DrawLine(self:GetPos(), v, color or defaultColor, true)
+
+        end
+
+    end
+
+end
+
+
+-- local functions for crating vertexes
+
+--[[
+    numVertexesCircle : Num
+
+    return table { [index] = Vector }
+]]
 local function createCircle(numVertexesCircle)
     
     local circleVertexes = {}
@@ -157,6 +359,11 @@ local function createCircle(numVertexesCircle)
 
 end
 
+--[[
+    circleVertexes : Table { [index] = Vector }
+
+    return table { [index] = Vector }
+]]
 local function createShereByCircle(circleVertexes)
 
     local shereVertexes = {}
@@ -182,9 +389,12 @@ local function createShereByCircle(circleVertexes)
     
 end
 
+
+-- private function
+
 function TRACE_BOMB:createCircle()
 
-    self.circleVertexes = createCircle(self.NumVertexesCircle)
+    self.circleVertexes = createCircle(self.numVertexesCircle)
 
 end
 
@@ -198,8 +408,8 @@ function TRACE_BOMB:shereSetup()
     
     local matrix = Matrix()
 
-    matrix:Translate(self.pos)
-    matrix:Scale(Vector(self.Radius, self.Radius, self.Radius))
+    matrix:Translate(self:GetPos())
+    matrix:Scale(Vector(self.radius, self.radius, self.radius))
     matrix:Rotate(self:GetSphereAxis():Angle())
 
     self.vertexes = {}
@@ -225,102 +435,80 @@ function TRACE_BOMB:createShere()
 
 end
 
-function TRACE_BOMB:UpdateVertexes()
+--[[
+    return this table
+]]
+function TRACE_BOMB:updateVertexes()
     
     self.vertexes = self:createShere()
     return self
 
 end
 
-function TRACE_BOMB:UpdateScale(k)
+--[[
+    lastPos : Num
+]]
+function TRACE_BOMB:updatePos(lastPos)
    
     local matrix = Matrix()
-    matrix:Scale(Vector(k, k, k))
+    matrix:Translate(self:GetPos() - lastPos)
 
+    self:multiplyByMatrix(matrix)
+
+end
+
+--[[
+    lastRadius : Num
+]]
+function TRACE_BOMB:updateScale(lastRadius)
+
+    local newRadius = self:GetRadius()
+
+    if newRadius > lastRadius then
+        
+        self:increaseSize(newRadius / lastRadius)
+        return
+
+    end
+
+    self:reduceSize(newRadius / lastRadius)
+    
+end
+
+--[[
+    num : Num
+]]
+function TRACE_BOMB:increaseSize(num)
+
+    local matrix = Matrix()
+    -- SetScale
+    matrix:Scale(Vector(num, num, num))
+
+    self:multiplyByMatrix(matrix)
+
+end
+
+--[[
+    num : Num
+]]
+function TRACE_BOMB:reduceSize(num)
+    
+    self:increaseSize(num > 0 and 1 / num or num - 1)
+
+end
+
+--[[
+    matrix : Matrix
+]]
+function TRACE_BOMB:multiplyByMatrix(matrix)
+    
     for k, v in ipairs(self.vertexes) do
         
         self.vertexes[k] = matrix * v
 
     end
-    
-end
-
-
-function TRACE_BOMB:Boom()
-    
-    if !self.vertexes then
-        self:UpdateVertexes()
-    end
-
-    local resultTable = self.takeEverything and {}
-
-    for _, v in ipairs(self.vertexes) do
-        
-        local trace = util.TraceLine({
-            
-            start = self.pos,
-            endpos = v,
-
-            filter = self.filter,
-        
-        })
-
-        if !(trace and trace.Hit) then continue end
-
-        if self.samplingConditionsFunc and !self.samplingConditionsFunc(trace, self.pos) then continue end
-
-        if !resultTable then return trace end
-
-        resultTable[#resultTable + 1] = trace 
-
-    end
-
-    return resultTable
 
 end
-
-
-if CLIENT then
-
-    function TRACE_BOMB:DrawShere(color)
-        
-        if !self.vertexes then return end
-
-        local last
-        for k, v in ipairs(self.vertexes) do
-            
-            if last then
-                render.DrawLine(last, v, color or defaultColor, true)
-            end
-
-            last = v
-
-        end
-
-
-    end
-    
-    function TRACE_BOMB:DrawPaintedShere(color)
-        
-        render.SetColorMaterial()
-        render.DrawSphere(self.pos, self.Radius, self.NumVertexesCircle, self.NumVertexesCircle, color or defaultColor)
-
-    end
-
-    function TRACE_BOMB:DrawTraces(color)
-    
-        if !self.vertexes then return end
-
-        for _, v in ipairs(self.vertexes) do
-            
-            render.SetColorMaterial()
-            render.DrawLine(self.pos, v, color or defaultColor, true)
-
-        end
-
-    end
-
-end
-
+ 
 
 ju.mobility_gear.classes.trace_bomb = TRACE_BOMB
